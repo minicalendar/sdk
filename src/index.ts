8a88c70f8d7a245d16458f1ff067c9674916ab14
Jack Kirkham <jack@minisquare.com>
2026-03-03 16:14:32 +0000

    fix: include pause in exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { getPortalUrl, type PortalParams, type PortalResponse } from "./portal.js"
 export { getConfig, type ConfigParams, type ConfigResponse } from "./config.js"
+export { pause, type PauseParams, type PauseResponse } from "./pause.js"
